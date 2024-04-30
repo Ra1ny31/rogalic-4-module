150799c5deab30fa8195bb89ac3c9d562e33eb2b
Ra1ny31 <goncharov.kirill.2009@mail.ru>
2024-04-30 18:18:41 +0300

    0.0.4.3
    добавлен урон от шипов

---
+++ Assets/Scripts/ThornsScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThornsScript : MonoBehaviour
+{
+    public float Damage = 100;
+    public PlayerHP HpMain;
+
+    private void Start()
+    {
+        HpMain = GetComponent<PlayerHP>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            HpMain.HP -= Damage;
+        }
+    }
+}
