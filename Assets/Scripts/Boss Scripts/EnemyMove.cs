ea89fc8d7a4d0af719240ea68409140b3b2f0d3d
Semen027 <159647166+Semen027@users.noreply.github.com>
2024-05-14 21:48:46 +0400

    1111111
    босс

---
+++ Assets/Scripts/Boss Scripts/EnemyMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+
+    public float stoppingDistance = 5f;
+    public GameObject player;
+    public GameObject vrag;
+    public float speed;
+
+    void Update()
+    {      
+        Vector2 player1 = player.transform.position;
+        Vector2 vrag1 = vrag.transform.position;
+        player1.y = vrag1.y;
+
+        if (Vector2.Distance(player1, vrag1) > stoppingDistance)
+        {
+            vrag.transform.position = Vector2.MoveTowards(vrag1, player1, speed * Time.deltaTime);
+        }
+    }
+    
+}
